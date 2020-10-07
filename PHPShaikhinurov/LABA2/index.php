610b1eb6e25d62f3e0a65ad68a6032a65f23567c
FosterLay <57025732+FosterLay@users.noreply.github.com>
2020-10-07 13:20:49 +0500

    Create index.php

---
+++ PHPShaikhinurov/LABA2/index.php
@@ -0,0 +1,10 @@
+<p> Дата и время:
+<p>
+<?php
+	$d=date("d.m.Y H:i");
+	echo($d);
+?>
+<ul id="nav">
+	<li><a href="lab21.php">Упражнение 1 (В8)</a>
+	<li><a href="lab22.php">Упражнение 2 (В6)</a>
+</ul>
