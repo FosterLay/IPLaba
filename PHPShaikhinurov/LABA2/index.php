5ddac9042033b518811f078e36be9887ea42f4a4
FosterLay <57025732+FosterLay@users.noreply.github.com>
2020-10-19 19:10:24 +0500

    Update index.php

--- PHPShaikhinurov/LABA2/index.php
+++ PHPShaikhinurov/LABA2/index.php
@@ -5,6 +5,11 @@
 	echo($d);
 ?>
 <ul id="nav">
-	<li><a href="lab21.php">Упражнение 1 (В8)</a>
-	<li><a href="lab22.php">Упражнение 2 (В6)</a>
+	<li><a href="lab241.php">Упражнение 4 (В1)</a>
+	<li><a href="lab242.php">Упражнение 4 (В8)</a>
+	<li><a href="lab251.php">Упражнение 5 (В1)</a>
+	<li><a href="lab252.php">Упражнение 5 (В8)</a>
+	<li><a href="lab261.php">Упражнение 6 (В1)</a>
+	<li><a href="lab262.php">Упражнение 6 (В6)</a>
+	<li><a href="lab262.php">Упражнение 6 (В8 - не выполненный)</a>
 </ul>
