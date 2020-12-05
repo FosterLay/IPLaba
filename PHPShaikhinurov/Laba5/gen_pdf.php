9a27a5f49c35fd8ed29b2795decbc04629f20739
Тимур <fosterygaty@yandex.ru>
2020-12-05 18:17:20 +0500

    Минорные обновления

--- PHPShaikhinurov/Laba5/gen_pdf.php
+++ PHPShaikhinurov/Laba5/gen_pdf.php
@@ -1,7 +1,7 @@
 <?php
     require('tfpdf/tfpdf.php');
 
-    $mysqli = new mysqli("eu-cdbr-west-03.cleardb.net", "be979b4b739385", "67d2bc8a", "heroku_59a01e27452dafc");
+    $mysqli = new mysqli("eu-cdbr-west-03.cleardb.net", "b0ce4d11992198", "ad55ff31", "heroku_955ee9896fe710e");
     if ($mysqli->connect_errno) {
         echo "Не удалось подключиться к БД";
     }
@@ -12,33 +12,32 @@
     $pdf->AddPage();
 
     $pdf->Cell(80);
-    $pdf->Cell(30,10,'Игры',1,0,'C');
+    $pdf->Cell(30,10,'Автомобили',1,0,'C');
     $pdf->Ln(20);
 
     $pdf->SetFillColor(200,200,200);
     $pdf->SetFontSize(6);
 
-    $header = array("п/п","Название","Жанр","Разработчик","Издатель","Ключ","Дата приобретения","Дата окончания","URL магазина");
-    $w = array(6,30,25,25,25,20,20,17,25);
+    $header = array("№ п/п","Марка","Модель","Год выпуска","Трансмиссия","Стоимость","Название автосалона","Адрес");
+    $w = array(6,30,25,25,25,20,20,25);
     $h = 10;
-    for ($c = 0; $c < 9; $c++){
+    for ($c = 0; $c < 8; $c++){
         $pdf->Cell($w[$c],$h,$header[$c],'LRTB','0','',true);
     }
     $pdf->Ln();
 
     // Запрос на выборку сведений о пользователях
     $result = $mysqli->query("SELECT
-        games.name as game_name,
-        games.genre as game_genre,
-        games.developer as game_developer,
-        games.publisher as game_publisher,
-        auto_in_dealer.key_code,
-        auto_in_dealer.purchase_date,
-        auto_in_dealer.expiry_date,
-        autodealer.url as store_url
+        automobiles.Brand as Brand,
+        automobiles.models as models,
+        automobiles.date_release as date_release,
+        automobiles.transmission as transmission,
+        automobiles.price as price,
+        autodealer.name as name,
+        autodealer.mail as mail
         FROM auto_in_dealer
-        LEFT JOIN games ON auto_in_dealer.game_id=games.id
-        LEFT JOIN autodealer ON auto_in_dealer.store_id=autodealer.id"
+        LEFT JOIN automobiles ON auto_in_dealer.idauto=automobiles.id
+        LEFT JOIN autodealer ON auto_in_dealer.iddealer=autodealer.id"
     );
 
     if ($result){
@@ -48,7 +47,10 @@
             $pdf->Cell($w[0],$h,$counter,'LRBT','0','C',true);
             $pdf->Cell($w[1],$h,$row[0],'LRB');
 
-            for ($c = 2; $c < 9; $c++){
+            for ($c = 2; $c < 8; $c++){
+                if ($c == 3){
+                    $row[2] = date('d-m-Y' , strtotime($row[2]));
+                }
                 $pdf->Cell($w[$c],$h,$row[$c-1],'RB');
             }
             $pdf->Ln();
--- PHPShaikhinurov/Laba5/gen_xls.php
+++ PHPShaikhinurov/Laba5/gen_xls.php
@@ -2,25 +2,24 @@
     require_once('php_excel/Classes/PHPExcel.php');
     require_once('php_excel/Classes/PHPExcel/Writer/Excel2007.php');
 
-    $mysqli = new mysqli("eu-cdbr-west-03.cleardb.net", "be979b4b739385", "67d2bc8a", "heroku_59a01e27452dafc");
+    $mysqli = new mysqli("eu-cdbr-west-03.cleardb.net", "b0ce4d11992198", "ad55ff31", "heroku_955ee9896fe710e");
     if ($mysqli->connect_errno){
         echo "Не удалось подключиться к БД";
     }
 
     // Запрос на выборку сведений о пользователях
     $result = $mysqli->query("SELECT
-        games.name as game_name,
-        games.genre as game_genre,
-        games.developer as game_developer,
-        games.publisher as game_publisher,
-        auto_in_dealer.key_code,
-        auto_in_dealer.purchase_date,
-        auto_in_dealer.expiry_date,
-        autodealer.url as store_url
+        automobiles.Brand as Brand,
+        automobiles.models as models,
+        automobiles.date_release as date_release,
+        automobiles.transmission as transmission,
+        automobiles.price as price,
+        autodealer.name as name,
+        autodealer.mail as mail
         FROM auto_in_dealer
-        LEFT JOIN games ON auto_in_dealer.game_id=games.id
-        LEFT JOIN autodealer ON auto_in_dealer.store_id=autodealer.id"
-    );
+        LEFT JOIN automobiles ON auto_in_dealer.idauto=automobiles.id
+        LEFT JOIN autodealer ON auto_in_dealer.iddealer=autodealer.id
+    ");
 
     $xls = new PHPExcel();
     // Устанавливаем индекс активного листа
@@ -28,19 +27,19 @@
     // Получаем активный лист
     $sheet = $xls->getActiveSheet();
     // Подписываем лист
-    $sheet->setTitle('Игры');
+    $sheet->setTitle('Автомобили');
     // Вставляем текст в ячейку A1
-    $sheet->setCellValue("A1", 'Игры');
+    $sheet->setCellValue("A1", 'Автомобили');
     $sheet->getStyle('A1')->getFill()->setFillType(PHPExcel_Style_Fill::FILL_SOLID);
     $sheet->getStyle('A1')->getFill()->getStartColor()->setRGB('EEEEEE');
     // Объединяем ячейки
-    $sheet->mergeCells('A1:I1');
+    $sheet->mergeCells('A1:H1');
     // Выравнивание текста
     $sheet->getStyle('A1')->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
 
     $c = 0;
 
-    $header = array("п/п","Название","Жанр","Разработчик","Издатель","Ключ","Дата приобретения","Дата окончания","URL магазина");
+    $header = array("№ п/п","Марка","Модель","Год выпуска","Трансмиссия","Стоимость","Название автосалона","Адрес");
     foreach ($header as $h){
         $sheet->setCellValueByColumnAndRow($c,2,$h);
         // Применяем выравнивание
@@ -58,6 +57,9 @@
             $c++;
 
             foreach ($row as $cell){
+                if ($c == 3){
+                    $cell = date('d-m-Y' , strtotime($cell));
+                }
                 $sheet->setCellValueByColumnAndRow($c,$r,$cell);
                 $c++;
             }
@@ -66,7 +68,7 @@
     }
 
     header('Content-Type: application/xls');
-    header('Content-Disposition: inline; filename=Games.xls');
+    header('Content-Disposition: inline; filename=Automobiles.xls');
     header('Cache-Control: private, max-age=0, must-revalidate');
     header('Pragma: public');
 
