5ded5bc468745e2839ef51c4e254c7fa57d1dd8f
FosterLay <57025732+FosterLay@users.noreply.github.com>
2021-06-02 22:20:00 +0500

    Add files via upload
    Добавлены 1 и 2 лабораторные работы

---
+++ JSShaikhinurov/index.php
@@ -0,0 +1,14 @@
+<html>
+    <p> Список лабораторных </p>
+
+    <ul id="nav">
+        <li><a href="Lab1">Лаба 1</a>
+        <li><a href="Lab2">Лаба 2</a>
+        <li><a href="Lab3">Лаба 3</a>
+        <li><a href="Lab4">Лаба 4</a>
+        <li><a href="Lab5">Лаба 5</a>
+
+        <li><a href="old">Интернет-программирование</a>
+    </ul>
+
+</html>
