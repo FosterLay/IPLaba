52ded1653d13c954d7001808e243f9a93b0e8f88
FosterLay <57025732+FosterLay@users.noreply.github.com>
2021-06-03 00:19:25 +0500

    Add files via upload

---
+++ JSShaikhinurov/Lab3/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <title>Лаврентьева Е.А.</title>
+<meta charset="utf-8" />
+</head>
+<body>
+<p>
+<ul>
+	<li><a href="3lab_1.html">Задание 1</a></li>
+	<li><a href="3lab_2.html">Задание 2</a></li>
+</ul>
+</p>
+</body>
+</html>
---
+++ JSShaikhinurov/Lab3/serials.php
@@ -0,0 +1,32 @@
+﻿<?php
+    $mysqli = new mysqli("localhost", "f0526324_serials", "0000", "f0526324_serials");
+    if ($mysqli->connect_errno) {
+        echo "Не удалось подключиться к БД";
+    }
+
+    $get_s = $_GET['get_s'];
+    $need_to_get = $_GET['need_to_get'];
+
+    $result = $mysqli->query(
+        "SELECT  id_s, name, des, pic
+        FROM serials ORDER BY id_s DESC LIMIT $get_s, $need_to_get
+        "
+    );
+
+    if ($result){
+        while ($row = $result->fetch_array()){
+            $id_s = $row['id_s'];
+            $name = $row['name'];
+            $des = $row['des'];
+            $pic = $row['pic'];
+
+            echo "<p>";
+            echo "<h1>$name</h1>";
+            echo "<img width='270px' height='385px' class='center-img' src='$pic'>";
+            echo "<p>";
+            echo "</img>";
+            echo $des;
+            echo "</p>";
+        }
+    }
+?>
