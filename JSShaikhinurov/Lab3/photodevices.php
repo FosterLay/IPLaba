4dd79ad7f9c1c94a06fc289d5d0063922667839f
FosterLay <57025732+FosterLay@users.noreply.github.com>
2021-06-03 00:36:32 +0500

    Update and rename serials.php to photodevices.php

--- JSShaikhinurov/Lab3/photodevices.php
+++ JSShaikhinurov/Lab3/photodevices.php
@@ -1,5 +1,5 @@
 ﻿<?php
-    $mysqli = new mysqli("localhost", "f0526324_serials", "0000", "f0526324_serials");
+    $mysqli = new mysqli("eu-cdbr-west-03.cleardb.net", "b0ce4d11992198", "ad55ff31", "heroku_955ee9896fe710e");
     if ($mysqli->connect_errno) {
         echo "Не удалось подключиться к БД";
     }
