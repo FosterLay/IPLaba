bbdcdfe15c1bdfd66fbc2127019cab8acb7436d5
FosterLay <57025732+FosterLay@users.noreply.github.com>
2021-06-02 22:34:20 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+    <p> Список лаб </p>
+
+    <ul id="nav">
+        <li><a href="JSShaikhinurov">Лаба JS</a>
+
+        <li><a href="old">Интернет-программирование</a>
+    </ul>
+
+</html>
